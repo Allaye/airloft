c98c97dd7d4257cca6f71ce23926c5912b6e89b3
Allaye <kolade199@gmail.com>
2022-01-31 18:01:01 +0100

    implements user registration view

--- airloft/account/views.py
+++ airloft/account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.generics import GenericAPIView
+from rest_framework import (response, status, permissions)
+from account.serializers import RegisterApiViewSerializer, LoginApiViewSerializer
 
-# Create your views here.
+# Create your api views here.
+class RegisterApiView(GenericAPIView):
+    '''
+    Create a new project object, and return the created object.
+    permission_classes = "IsAuthenticated" :user is logged in , "IsAdminUser" : user is admin
+    Post: /api/register
+    {
+    "username": "ab",
+    "email": "ab@email.com",
+    "password": "ab@email.com"
+    }
+
+    response:
+    {
+    "id": 1,
+    "username": "ab",
+    "email": "ab@email.com",
+    "is_staff": false
+    }
+    '''
+    authentication_classes = []
+    serializer_class = RegisterApiViewSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
