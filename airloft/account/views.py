e418b1292a06d72d0f97d60815d8fe3d409ebfa6
Allaye <kolade199@gmail.com>
2022-01-31 18:27:16 +0100

    implements user authentication view

--- airloft/account/views.py
+++ airloft/account/views.py
@@ -33,6 +33,7 @@ class RegisterApiView(GenericAPIView):
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LoginApiView(GenericAPIView):
     authentication_classes = []
     serializer_class = LoginApiViewSerializer
@@ -46,3 +47,13 @@ class LoginApiView(GenericAPIView):
             serializer = self.serializer_class(user)
             return response.Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return response.Response({'message': 'Bad login credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthUserApiView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        serializer = RegisterApiViewSerializer(user)
+        return response.Response({'user': serializer.data}, status=status.HTTP_202_ACCEPTED)
