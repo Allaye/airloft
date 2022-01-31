44724a0a472d7d02c46bd633935a08b1d34e96ba
Allaye <kolade199@gmail.com>
2022-01-31 18:51:33 +0100

    writing test cases for the account app

--- airloft/account/tests.py
+++ airloft/account/tests.py
@@ -24,3 +24,22 @@ class TestEmployeeModelUseCase(APITestCase):
     def test_raises_error_with_message_when_no_email_is_supplied(self):
         with self.assertRaisesMessage(ValueError, 'The email must be set'):
             User.objects.create_user(username='moti', email='', password='password123!@')
+
+
+class TestEmployeeUseCase(APITestCase):
+    """test case class to test the employee/user endpoints"""
+    
+    def test_register_new_user(self):
+        """test case to test if a user can create account with the endpoint"""
+        
+        account_creation_data = {
+            'username': 'admin',
+            'password': 'admin',
+            'email': 'admin@user.com',
+            'is_staff': 1
+        }
+        response = self.client.post(reverse("register"), account_creation_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+
+    
