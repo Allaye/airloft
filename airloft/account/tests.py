dc9beae5b8fb8b7ea265e61f41cdefbe4b259929
Allaye <kolade199@gmail.com>
2022-01-31 18:40:44 +0100

    writing test cases for the account app

--- airloft/account/tests.py
+++ airloft/account/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from account.models import User
 
-# Create your tests here.
+
+class TestEmployeeModelUseCase(APITestCase):
+
+    def test_creates_user(self):
+        user = User.objects.create_user('moti', 'moti@gmail.com', 'password123!@', is_staff=False)
+        self.assertIsInstance(user, User)
+        self.assertFalse(user.is_staff)
+        self.assertEqual(user.email, 'moti@gmail.com')
