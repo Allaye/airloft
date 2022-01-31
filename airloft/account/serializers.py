2bb883e99c575b8701df8b9e3fc0ae16bb580d4e
Allaye <kolade199@gmail.com>
2022-01-31 17:47:38 +0100

    added serializers to account app

--- airloft/account/serializers.py
+++ airloft/account/serializers.py
@@ -12,3 +12,13 @@ class RegisterApiViewSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class LoginApiViewSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'token', 'is_staff')
+
+        read_only_fields = ['token']
