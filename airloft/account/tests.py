ed55ef09f6a4b862b8c941ccf5c3dea04f8a93ed
Allaye <kolade199@gmail.com>
2022-01-31 18:47:39 +0100

    writing test cases for the account app

--- airloft/account/tests.py
+++ airloft/account/tests.py
@@ -11,3 +11,16 @@ class TestEmployeeModelUseCase(APITestCase):
         self.assertIsInstance(user, User)
         self.assertFalse(user.is_staff)
         self.assertEqual(user.email, 'moti@gmail.com')
+
+    def test_raises_error_when_no_username_is_supplied(self):
+        self.assertRaises(ValueError, User.objects.create_user, username="",
+                          email='moti@gmail.com', password='password123!@')
+
+    def test_raises_error_with_message_when_no_username_is_supplied(self):
+        with self.assertRaisesMessage(ValueError, 'The given username must be set'):
+            User.objects.create_user(
+                username='', email='moti@gmail.com', password='password123!@')
+
+    def test_raises_error_with_message_when_no_email_is_supplied(self):
+        with self.assertRaisesMessage(ValueError, 'The email must be set'):
+            User.objects.create_user(username='moti', email='', password='password123!@')
