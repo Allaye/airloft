842ba54d0bbd97045af2475870cac46ab0771311
Allaye <kolade199@gmail.com>
2022-02-04 18:42:14 +0100

    cleaned up model.py

--- airloft/fleet/models.py
+++ airloft/fleet/models.py
@@ -1,12 +1,10 @@
 import uuid
-from datetime import datetime, timedelta, tzinfo
-from django.utils import timezone
+from datetime import datetime, timedelta
 from django.db import models
 from django.forms import ValidationError
-from django.utils.functional import cached_property
+from utils.icao import ICAO
 
 # from account.models import User
-from utils.icao import ICAO
 
 # Create your models here.
 
@@ -33,8 +31,8 @@ def validate_future_flight(date):
     from the date it was created.
     """
     now = datetime.now().replace(tzinfo=None).strftime("%Y-%m-%d %H:%M:%S")
-    print(date.replace(tzinfo=None)+ timedelta(minutes=33))
-    print(datetime.fromisoformat(now))
+    # print(date.replace(tzinfo=None)+ timedelta(minutes=33))
+    # print(datetime.fromisoformat(now))
     if date.replace(tzinfo=None) > datetime.fromisoformat(now) + timedelta(days=1):
         return True
     else:
@@ -57,24 +55,11 @@ class Aircraft(models.Model):
     serial_number = models.CharField(max_length=100, unique=True, default=generate_uuid)
     manufacturer = models.CharField(blank=False, null=False, max_length=200)
     model = models.TextField(blank=False, null=False, max_length=400, default="")
-    # technology = models.JSONField()
-    # members = models.ManyToManyField(User, related_name='all_members')
-    # start_date = models.DateField(default=date.today())
-    # end_date = models.DateField(blank=True, null=True)
+   
 
     def __str__(self):
         return f"{self.manufacturer} {self.model}"
-    # @property
-    # def is_completed(self):
-    #     """
-    #     check if the current project has been completed and closed
-    #     """
-    #     if self.end_date is None:
-    #         return False
-    #     return self.end_date < datetime.now().date()
-
-    # class Meta:
-    #     ordering = ['start_date']
+    
 
 
 class Airport(models.Model):
@@ -93,10 +78,7 @@ class Airport(models.Model):
         """
         self.code = generate_icao(self.name)
         super(Airport, self).save(*args, **kwargs)
-        # if self.code is None:
-        #     self.code = generate_icao(self.name)
-        # super().save(*args, **kwargs)
-
+       
 
 class Flight(models.Model):
     id = models.AutoField(primary_key=True)
@@ -107,7 +89,6 @@ class Flight(models.Model):
     # user = models.ForeignKey(to=Employee, on_delete=models.CASCADE)
     departure_time = models.DateTimeField(validators=[validate_future_flight])
     arrival_time = models.DateTimeField(blank=False, null=False)
-    # activity_duration = models.DurationField(blank)
 
     def clean(self) -> None:
         if self.arrival_time is None:
@@ -125,40 +106,3 @@ class Flight(models.Model):
         self.full_clean()
         super(Flight, self).save(*args, **kwargs)
         
-    # def __str__(self):
-    #     """
-    #     convert to a string representation
-
-    #     Returns:
-    #         string: string representation of the object
-    #             " <title>  : <project> : <user> : <startdate>  : <enddate>"
-    #     """
-    #     if self.end_time is None:
-    #         return f"{self.description} : {self.project.title} : {self.user.username} : {self.start_time} : {'Activity in progress'}"
-    #     else:
-    #         return f"{self.description} : {self.project.title} : {self.user.username} : {self.start_time} : {self.end_time}"
-
-    # @property
-    # def is_running(self):
-    #     """
-    #     check if the activity is running
-
-    #     Returns:
-    #         bool: True if the activity is running, False otherwise
-    #     """
-    #     return self.end_time is None
-
-    # @cached_property
-    # def duration(self):
-    #     """
-    #     get the duration of the activity
-
-    #     Returns:
-    #         timedelta: duration of the activity
-    #     """
-    #     if self.is_running:
-    #         sec = datetime.now(timezone.utc) - self.start_time
-    #         return str(timedelta(seconds=round(sec.total_seconds())))
-    #     else:
-    #         sec = self.end_time - self.start_time
-    #         return str(timedelta(seconds=round(sec.total_seconds())))
