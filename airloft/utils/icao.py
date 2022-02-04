f3c53773c34fc009946a056c740bd6314094e0b2
Allaye <kolade199@gmail.com>
2022-02-04 19:13:37 +0100

    added utls files

---
+++ airloft/utils/icao.py
@@ -0,0 +1,15 @@
+ICAO = {
+    "Anaa": "NTGA",
+    "Apalachicola Regional": "KAAF",
+    "Malamala": "FAMD",
+    "Al Ain International": "OMAL",
+    "Atlantic City": "KACY",
+    "Albany International": "KBAL",
+    "Baise Youjiang": "ZGBS",
+    "Albuquerque International Sunport": "KABQ",
+    "RAF Abisko": "EAAK",
+    "RAF Leuchars": "EGQL",
+    "Santiago de Compostela": "LEST",
+    "Seve Ballesteros-Santander": "LEXJ",
+}
+
